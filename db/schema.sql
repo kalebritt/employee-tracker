694883673cbb90d7aba68cec23aa695e5dd3045b
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-10 11:25:47 -0600

    created department table in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,4 +5,14 @@ CREATE DATABASE employee_db;
 --use!
 USE employee_db;
 
+DROP TABLE IF EXISTS roles;
+USE TABLE roles;
+
+--this here table's for the deparment 
+CREATE TABLE deparment (
+    name VARCHAR(30) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+)
+
 
