676442e8c2a184ecfa413fc7cb78257e9f2dbe83
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-10 08:25:10 -0600

    connection to db and comments in server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-const cTable = require('console.table');
-const mysql = require('mysql2');
+//dependancies
+const cTable = require("console.table");
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
+
+//connection to database created
+const connection = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    database: "employee_db",
+  },
+  console.log("connected, friendo, to employe_db")
+);
