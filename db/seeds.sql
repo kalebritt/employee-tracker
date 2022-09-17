1e7cff7b40d93b23cdd2137b27b99525619b85de
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-17 08:48:06 -0600

    added comments to seeds.sql and first line of roles and employee seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+--deparement seeds
 INSERT INTO deparment (department_name)
 VALUES
 ('Sales'),
@@ -9,3 +10,10 @@ VALUES
 ('Customer Service'),
 ('Other');
 
+--roles seeds
+INSERT INTO roles (title, salary, deparment_id)
+VALUES 
+
+-- employee seeds
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
