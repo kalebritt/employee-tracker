246a76eacd73462257fbc27134f8f7aa02da4768
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-17 09:05:03 -0600

    reorder engineering department to 2 and started work on roles seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,9 +2,9 @@
 INSERT INTO deparment (department_name)
 VALUES
 ('Sales'),
+('Engineering'),
 ('Legal'),
 ('Marketing'),
-('Engineering'),
 ('Finance'),
 ('Human Resources'),
 ('Customer Service'),
@@ -13,6 +13,10 @@ VALUES
 --roles seeds
 INSERT INTO roles (title, salary, deparment_id)
 VALUES 
+('Sales Lead', '100000', 1),
+('Salesperson', '80000', 1),
+('Lead Engineer', '150000', 2),
+('Software Engineer', '120000', 2),
 
 -- employee seeds
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
