0bdcaf5cf77542a1b6459396561989c7db2e49c1
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-17 09:11:00 -0600

    moved finance to 3 position and created finance and legal seeds in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,9 +3,9 @@ INSERT INTO deparment (department_name)
 VALUES
 ('Sales'),
 ('Engineering'),
+('Finance'),
 ('Legal'),
 ('Marketing'),
-('Finance'),
 ('Human Resources'),
 ('Customer Service'),
 ('Other');
@@ -17,6 +17,11 @@ VALUES
 ('Salesperson', '80000', 1),
 ('Lead Engineer', '150000', 2),
 ('Software Engineer', '120000', 2),
+('Account Manager', '160000', 3),
+('Accountant', '125000', 3),
+('Legal Team Lead', '250000', 4),
+('Lawyer', '190000', 4),
+
 
 -- employee seeds
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
