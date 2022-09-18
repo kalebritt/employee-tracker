58023bb6d5cc6c055f597efe9c8d29c71968709f
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-18 12:14:05 -0600

    formatted connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,17 +2,17 @@ const mysql = require("mysql2");
 
 //console.log to see if connected to database
 const connection = mysql.createConnection(
-    {
-      host: "localhost",
-      user: "root",
-      database: "employee_db",
-      password: "xqa^NhYJO1#yQ5X%UfD",
-    },
-    console.log("connected, friendo, to employe_db")
-  );
-  
-  connection.connect(function(err) {
-    if(err) throw err;
-  });
+  {
+    host: "localhost",
+    user: "root",
+    database: "employee_db",
+    password: "xqa^NhYJO1#yQ5X%UfD",
+  },
+  console.log("connected, friendo, to employe_db")
+);
 
-  module.exports = connection
+connection.connect(function (err) {
+  if (err) throw err;
+});
+
+module.exports = connection;
