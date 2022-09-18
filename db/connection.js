80df12098453312ebd7fe3628189486448bb2ae8
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-18 12:13:50 -0600

    defining connection in connection.js file

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,18 @@
 const mysql = require("mysql2");
 
 //console.log to see if connected to database
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "employee_db",
-  password: "xqa^NhYJO1#yQ5X%UfD",
-});
+const connection = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      database: "employee_db",
+      password: "xqa^NhYJO1#yQ5X%UfD",
+    },
+    console.log("connected, friendo, to employe_db")
+  );
+  
+  connection.connect(function(err) {
+    if(err) throw err;
+  });
+
+  module.exports = connection
