01c59f9e9ddf41b53bd1535d8d0296f0fa439b3e
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-18 12:06:02 -0600

    tidied server.js, removed express and renamed to index.js

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 //dependancies
-const express = require("express");
 const cTable = require("console.table");
 const mysql = require("mysql2");
 // const inquirer = require("inquirer");
-const PORT = process.env.PORT || 3001;
-//express
-const app = express();
+
 //figlet fun
 const figlet = require("figlet");
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 //console.log to see if figlet connected
 figlet("Hello World!!", function (err, data) {
@@ -29,11 +22,7 @@ const connection = mysql.createConnection(
     host: "localhost",
     user: "root",
     database: "employee_db",
+    password: "xqa^NhYJO1#yQ5X%UfD",
   },
   console.log("connected, friendo, to employe_db")
 );
-
-//listnerapp
-app.listen(PORT, () => {
-  console.log(`here we go again at ${PORT}`);
-});
