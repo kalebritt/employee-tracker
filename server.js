8fb899576280d5a4600ce3d9f893a17a771f14e4
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-18 11:16:55 -0600

    added listener app and formatted server.js

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 //dependancies
-const express = require('express');
+const express = require("express");
 const cTable = require("console.table");
 const mysql = require("mysql2");
 // const inquirer = require("inquirer");
 const PORT = process.env.PORT || 3001;
 //express
 const app = express();
+//figlet fun
+const figlet = require("figlet");
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//figlet fun
-const figlet = require('figlet');
-
 //console.log to see if figlet connected
-figlet('Hello World!!', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
+figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
 });
-
 
 //console.log to see if connected to database
 const connection = mysql.createConnection(
@@ -34,3 +32,8 @@ const connection = mysql.createConnection(
   },
   console.log("connected, friendo, to employe_db")
 );
+
+//listnerapp
+app.listen(PORT, () => {
+  console.log(`here we go again at ${PORT}`);
+});
